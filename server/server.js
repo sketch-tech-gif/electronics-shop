d8132b31a235a1b5b2a29c42e71dc79553b3c0c4
sketch-tech-gif <kangogo6074@gmail.com>
2026-04-09 13:04:42 +0300

    Fix duplicate cors import

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
