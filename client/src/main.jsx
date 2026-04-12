4ee1f48a4dfc408cb3c5168cb9a95b6af585314c
sketch-tech-gif <kangogo6074@gmail.com>
2026-04-12 15:27:27 +0300

    fix google oauth provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
-// FILE: src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="360074851790-0lr673nbbl2mj2jmb2fjb2tdkisbq22c.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
-);
+)
